732a15f3b14e73d47612ffaa10d9dea7707a3b5c
Corbin Gruber <corbingruber@Corbins-MacBook-Pro.local>
2024-06-11 22:33:19 +0100

    addition of updated resume and contact links to resume tab

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -53,12 +53,15 @@ const Resume = ({ isBookOpen, setIsBookOpen }) => {
                 <br />
                 <div className="flex justify-around mt-10">
                   <span className="border-2 border-dashed rounded-md border-gray-400 hover:border-gray-700 text-gray-500 hover:text-gray-700 p-5 ">
-                    <a>
+                    <a href="mailto: corbingrbr@gmail.com">
                       <i className={`self-center fa-xl fa-solid fa-envelope`} />
                     </a>
                   </span>
                   <span className="border-2 border-dashed rounded-md border-gray-400 hover:border-gray-700 text-gray-500 hover:text-gray-700 p-5">
-                    <a>
+                    <a
+                      href="https://www.instagram.com/corbingrbr/"
+                      target="_blank"
+                    >
                       <i
                         className={`self-center fa-xl fa-brands fa-instagram`}
                       />
@@ -66,7 +69,10 @@ const Resume = ({ isBookOpen, setIsBookOpen }) => {
                   </span>
 
                   <span className="border-2 border-dashed rounded-md border-gray-400 hover:border-gray-700 text-gray-500 hover:text-gray-700 p-5 ">
-                    <a>
+                    <a
+                      href="https://www.threads.net/@corbingrbr"
+                      target="_blank"
+                    >
                       <i className={`self-center fa-xl fa-brands fa-threads`} />
                     </a>
                   </span>
